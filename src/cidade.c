4675655d5d4f382a2537973bbd10277efa8c9c7d
LuizFXavier <zap422238@gmail.com>
2024-06-30 02:38:20 -0400

    Unificação dos diferentes tipos de cidade

--- src/cidade.c
+++ src/cidade.c
@@ -3,41 +3,26 @@
 #include <string.h>
 #include "cidade.h"
 
-cidade* cria_cidade(double x, double y){
-    cidade* nova = (cidade*) malloc(sizeof(cidade));
-
-    nova->longitude = x;
-    nova->latitude = y;
+Cidade *cria_cidade(char *ibge, char *nome, float longi, float lat, int capital, int uf, int siafi, int ddd, char *fuso)
+{
+    Cidade* nova = (Cidade*) malloc(sizeof(Cidade));
+    nova->codigo_ibge = ibge;
+    nova->nome = nome;
+    nova->longitude = longi;
+    nova->latitude = lat;
+    nova->capital = capital;
+    nova->codigo_uf = uf;
+    nova->siafi_id = siafi;
+    nova->ddd = ddd;
+    nova->fuso_horario = fuso;
 
     return nova;
 }
-int cmp_ddd(const void* a, const void* b){
-    return ((c_ddd*)a)->ddd - ((c_ddd*)b)->ddd;
-}
 
-int cmp_cod_UF(const void *a, const void *b)
+char *get_key(const void *mun)
 {
-    return ((c_cod_UF*)a)->codigo_uf - ((c_cod_UF*)b)->codigo_uf;
-}
 
-int cmp_lat(const void *a, const void *b)
-{
-    return (int)((c_lat*)a)->latitude - (int)((c_lat*)b)->latitude;
-}
-
-int cmp_long(const void *a, const void *b)
-{
-    return (int)((c_long*)a)->longitude - (int)((c_long*)b)->longitude;
-}
-
-int cmp_nome(const void *a, const void *b)
-{
-    return strcmp(((c_nome*)a)->nome, ((c_nome*)b)->nome);
-}
-
-char* get_key(const void* mun){
-
-    cidade* m = (cidade*)mun;
+    Cidade* m = (Cidade*)mun;
 
     return m->codigo_ibge;
 }
